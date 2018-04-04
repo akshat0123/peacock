753ffe819948b79e624a3634b2a5941047d97d9d
akshat0123 <akshat0123@gmail.com>
2018-04-04 15:00:49 -0400

    Initialized language model

--- language_model.py
+++ language_model.py
@@ -5,11 +5,28 @@ class LanguageModel:
     def __init__(self):
         """ Initializes language model
         """
-        pass
+        self.trigram_freqs = {}
+        self.bigram_freqs = {}
+        self.unigram_freqs = {}
 
    
     def add_document(self, text):
         """ Takes in a document as a string and adds it to the model
+        """
+        pass
+
+    
+    def generate_probability_matrix(words = None):
+        """ Takes in a list of previous words and returns a dictionary with each
+            of the possible following words and their probability of occurrence
+        """
+        pass
+
+
+    def generate_inv_probability_matrix(prob_matrix):
+        """ Takes in a probability matrix and returns a inverted matrix where
+            the probabilities of each word are the key and the words are the
+            value
         """
         pass
 
@@ -21,7 +38,7 @@ class LanguageModel:
         pass
 
 
-    def calculate_similarity(self, text):
+    def calculate_log_likelihood(self, text):
         """ Takes in a string and returns similarity value of the string and the
             language model
         """
