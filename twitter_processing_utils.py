03035a56a0426cf28d8c805a488dea9d1d17d60e
Hannah•Leung <hannah.leung@email.virginia.edu>
2018-04-04 22:17:17 -0400

    Remove URLs and keep letters only.

--- twitter_processing_utils.py
+++ twitter_processing_utils.py
@@ -1,5 +1,15 @@
 
 def process_tweet(raw):
-    """ Takes in raw tweet text and returns cleaned text
-    """
-    pass
+	""" Takes in raw tweet text and returns cleaned text
+	"""
+	# 1. replace the url with single space
+	rm_urls = re.sub('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'," ",raw)
+
+	# 2. removing the RT
+	# rm_RT = if this tweet contains RT then remove the whole tweet
+
+	# 3. remove the @ # ??
+	letters_only = re.sub("[^a-zA-Z]"," ",rm_RT)
+	# 4. Find out how the picture
+	# 5. emoji
+	pass
