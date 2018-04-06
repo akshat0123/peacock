cbbef93b36bd5f3a963e802197fff52c645a7127
Hannah•Leung <hannah.leung@email.virginia.edu>
2018-04-06 12:05:39 -0400

    credentials.

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 credentials = {
-	'username': '',
-	'consumer_key': '',
-	'consumer_secret': '',
-	'access_token': '',
-	'access_token_secret': ''
- }
+    'username': 'akshat0123',
+    'consumer_key': 'F77qZPb6kupm3N0HalcRlt3Ci',
+    'consumer_secret': 'SpQKFY5oHTQDxZLHf7lMg4xg2ce6DjjA6GA8IlsBGmASdTunOB',
+    'access_token': '1282087154-18iawgdXLTOZ6NnS0XkTOtFFXo4Ri7pVZ91ie9b',
+    'access_token_secret': 'A8EMI9KySmY5XlQPus099410APudMwn4wTIazkbjsaNHe'
+}
