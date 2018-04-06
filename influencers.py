8c290d895162cc0068efa0edb2ccff48a1b12e92
Hannah•Leung <hannah.leung@email.virginia.edu>
2018-04-06 12:08:14 -0400

    uppdate with 100 most influencers.

--- influencers.py
+++ influencers.py
@@ -1,1 +1,10 @@
-influencers = ['']
+
+
+# 1. selection 
+# 2. create deleted_list
+
+
+
+class Influencers():
+
+	influencers = ["katyperry","justinbieber","BarackObama","rihanna","taylorswift13","ladygaga","TheEllenShow","Cristiano","YouTube","jtimberlake","Twitter","KimKardashian","britneyspears","ArianaGrande","selenagomez","ddlovato","cnnbrk","shakira","jimmyfallon","realDonaldTrump","BillGates","JLo","Oprah","BrunoMars","narendramodi","nytimes","KingJames","MileyCyrus","CNN","NiallOfficial","instagram","neymarjr","BBCBreaking","Drake","SportsCenter","KevinHart4real","iamsrk","espn","LilTunechi","SrBachchan","wizkhalifa","Louis_Tomlinson","Pink","LiamPayne","BeingSalmanKhan","Harry_Styles","onedirection","aliciakeys","KAKA","realmadrid","NASA","Adele","EmmaWatson","ConanOBrien","FCBarcelona","chrisbrown","ActuallyNPH","NBA","danieltosh","pitbull","zaynmalik","KendallJenner","khloekardashian","akshaykumar","PMOIndia","sachin_rt","KylieJenner","coldplay","NFL","imVkohli","kourtneykardash","deepikapadukone","TheEconomist","aamir_khan","iHrithik","BBCWorld","POTUS","Eminem","andresiniesta8","NatGeo","MesutOzil1088","HillaryClinton","priyankachopra","AvrilLavigne","davidguetta","MohamadAlarefe","NICKIMINAJ","blakeshelton","MariahCarey","elonmusk","ChampionsLeague","ricky_martin","Google","edsheeran","arrahman","Reuters","AlejandroSanz","LeoDiCaprio","aplusk","Dr_alqarn"]
