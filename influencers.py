c1c14c6197c28dc28b70672577c3ef37e9e1dc70
akshat0123 <akshat0123@gmail.com>
2018-04-22 12:29:50 -0400

    Added methods in Language Model to determine the probability of a unigram, bigram, and a trigram being generated by the language model

--- influencers.py
+++ influencers.py
@@ -7,7 +7,7 @@ class Influencers:
 
     def __init__(self):
         self.allInfluencers = ["katyperry","justinbieber","BarackObama","rihanna","taylorswift13","ladygaga","TheEllenShow","Cristiano","YouTube","jtimberlake","Twitter","KimKardashian","britneyspears","ArianaGrande","selenagomez","ddlovato","cnnbrk","shakira","jimmyfallon","realDonaldTrump","BillGates","JLo","Oprah","BrunoMars","narendramodi","nytimes","KingJames","MileyCyrus","CNN","NiallOfficial","instagram","neymarjr","BBCBreaking","Drake","SportsCenter","KevinHart4real","iamsrk","espn","LilTunechi","SrBachchan","wizkhalifa","Louis_Tomlinson","Pink","LiamPayne","BeingSalmanKhan","Harry_Styles","onedirection","aliciakeys","KAKA","realmadrid","NASA","Adele","EmmaWatson","ConanOBrien","FCBarcelona","chrisbrown","ActuallyNPH","NBA","danieltosh","pitbull","zaynmalik","KendallJenner","khloekardashian","akshaykumar","PMOIndia","sachin_rt","KylieJenner","coldplay","NFL","imVkohli","kourtneykardash","deepikapadukone","TheEconomist","aamir_khan","iHrithik","BBCWorld","POTUS","Eminem","andresiniesta8","NatGeo","MesutOzil1088","HillaryClinton","priyankachopra","AvrilLavigne","davidguetta","MohamadAlarefe","NICKIMINAJ","blakeshelton","MariahCarey","elonmusk","ChampionsLeague","ricky_martin","Google","edsheeran","arrahman","Reuters","AlejandroSanz","LeoDiCaprio","aplusk","Dr_alqarn"]
-        self.infGroup = random.sample(self.allInfluencers, 1)
+        self.infGroup = random.sample(self.allInfluencers, 2)
         self.discard = self.infGroup
         self.infAvail = list(filter(lambda x: x not in self.infGroup, self.allInfluencers))
         
